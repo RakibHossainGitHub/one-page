0acc8300a1027e5853da1efb9562984f15083eb6
RakibHossainGitHub <rakib016477@gmail.com>
2023-09-02 01:40:10 +0600

    commit2

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,11 @@ window.onscroll = function () {
     nav.classList.remove("scroll-on");
   }
 };
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
